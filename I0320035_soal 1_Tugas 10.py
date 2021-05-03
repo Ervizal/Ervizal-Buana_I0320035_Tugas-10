aa04525f2a750d101fcf1ba9486876353cd20a3a
Ervizal Buana <Ervizal14@gmail.com>
2021-05-03 22:04:37 +0700

    Tugas 10

---
+++ I0320035_soal 1_Tugas 10.py
@@ -0,0 +1,12 @@
+print("=====================================")
+print("===== Program Pembuat Direktori =====")
+print("=====================================")
+
+import os
+
+def main():
+	os.mkdir("Biodata_prokom")
+	
+ifname = "main"
+
+main()
